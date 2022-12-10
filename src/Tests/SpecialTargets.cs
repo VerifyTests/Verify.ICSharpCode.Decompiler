99d696992675b759843928bd38aa5118f356e9d6
tom-englert <mail@tom-englert.de>
2022-12-10 11:22:39 +0100

    Fix #395: MethodToDisassemble can distinguish overloaded methods.

--- src/Tests/SpecialTargets.cs
+++ src/Tests/SpecialTargets.cs
@@ -1,3 +1,14 @@
+namespace MyNamespace
+{
+    class TypeInNamespace
+    {
+        class NestedType
+        {
+
+        }
+    }
+}
+
 public class GenericTarget<T1>
 {
     public void Method()
@@ -5,6 +16,18 @@ public class GenericTarget<T1>
     }
 
     public T1 GenericMethod1<T3>() => default!;
+
+    public void Overload()
+    {
+    }
+
+    public void Overload(string a, string b)
+    {
+    }
+
+    public void Overload(string a, double b)
+    {
+    }
 }
 
 public class GenericTarget<T1, T2>
