1b045027fd8820605cef6c0f47b92be505683d01
Simon Cropp <simon.cropp@gmail.com>
2023-06-05 21:44:21 +1000

    refs

--- src/Verify.ICSharpCode.Decompiler/VerifyICSharpCodeDecompiler.cs
+++ src/Verify.ICSharpCode.Decompiler/VerifyICSharpCodeDecompiler.cs
@@ -8,10 +8,6 @@ public static partial class VerifyICSharpCodeDecompiler
 
     public static bool Initialized { get; private set; }
 
-    [Obsolete("Use Initialize()")]
-    public static void Enable() =>
-        Initialize();
-
     public static void Initialize()
     {
         if (Initialized)
